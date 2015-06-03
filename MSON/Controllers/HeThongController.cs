5891914ca70a3d4ba76d364fdfb280cbee9b174c
Tran Bao Huu <tranbaohuu@gmail.com>
2015-06-03 15:07:08 +0700

    Khởi tạo

---
+++ MSON/Controllers/HeThongController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MSON.Controllers
+{
+    public class HeThongController : Controller
+    {
+        // GET: HeThong
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult SanPham()
+        {
+            return View();
+        }
+    }
+}
---
+++ MSON/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MSON.Startup))]
+namespace MSON
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
