4e550a3193f9b474382abbbeeed6b68fcfb3931c
Tran Bao Huu <tranbaohuu@gmail.com>
2015-06-05 01:37:49 +0700

    Hiển thị sản phẩm kềm Database Script

--- MSON/Controllers/HeThongController.cs
+++ MSON/Controllers/HeThongController.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MSON.Models;
 
 namespace MSON.Controllers
 {
     public class HeThongController : Controller
     {
+
+        private minhsondbEntities ett = new minhsondbEntities();
+
         // GET: HeThong
         public ActionResult Index()
         {
-            return View();
+
+            var model = ett.sanphams.Select(s => s);
+
+
+            return View(model);
         }
 
 
@@ -19,5 +28,28 @@ namespace MSON.Controllers
         {
             return View();
         }
+
+
+        public ActionResult LayDanhSach()
+        {
+
+            //var model = ett.sanphams.Select(s => s);
+
+
+            var model = from sp in ett.sanphams
+
+                        select new KieuCuaToiModels()
+                        {
+                            TenHang = sp.TEN,
+                            NgayNhap = sp.NGAYNHAP ?? default(DateTime)
+
+
+                        };
+
+
+
+            return View(model.ToList());
+        }
+
     }
 }
---
+++ MSON/Models/KieuCuaToiModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MSON.Models
+{
+    public class KieuCuaToiModels
+    {
+
+        public string TenHang { get; set; }
+
+        public DateTime NgayNhap { get; set; }
+
+
+    }
+}
